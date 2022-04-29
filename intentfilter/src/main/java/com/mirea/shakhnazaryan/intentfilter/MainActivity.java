57776f8752f578a55add83b0c22d779fab132815
adder <temartem68@mail.ru>
2022-04-29 14:06:12 +0300

    Practice 2

--- intentfilter/src/main/java/com/mirea/shakhnazaryan/intentfilter/MainActivity.java
+++ intentfilter/src/main/java/com/mirea/shakhnazaryan/intentfilter/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void onClickButton(View view) {
         Uri address = Uri.parse("https://www.mirea.ru/");
         Intent openLinkIntent = new Intent(Intent.ACTION_VIEW, address);
         if (openLinkIntent.resolveActivity(getPackageManager()) != null) {
